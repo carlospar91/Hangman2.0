7a65f9e9e727d850a176ee76f0e2c5f479a0f8b6
carlospar91 <carlospar91@aol.com>
2019-04-02 20:52:45 -0500

    Update HangmanProject.java

--- HangmanProject.java
+++ HangmanProject.java
@@ -6,115 +6,40 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-/*
- * @author NicoleBritt
+/**
+ *
+ * @author CARLOSPARLOUR
  */
-public class GuessWord {
+public class HangmanProject {
 
-    String secretWord;
-    int attempts = 6;
-    int position = 0;
-    int lettersLeft;
-    char[] hidden;
-    char[] guessedLetters = new char[26];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int choice1;
 
-    public GuessWord(String word) {
-        secretWord = word.toLowerCase();
-        hidden = new char[secretWord.length()];
-        lettersLeft = secretWord.length();
-    }
+        System.out.println("Please choose a topic: 1 for cars, 2 for presidents, 3 for a random topic."
+                + "\nIf your input is invalid we will choose a word for you!");
+        Scanner sc1 = new Scanner(System.in);
+        choice1 = sc1.nextInt();
+        //sc1.close(); //@Nicole Deleted scanner close due to error: NoSuchElementException
 
-    
-    public void Guessing() {
-        int lettersLeft = secretWord.length();
-//      char[] hidden = new char[secretWord.length()];
-        spaces();
-        System.out.println(attempts);
+        
+        
+        RandomWord randW = new RandomWord(choice1);
+        randW.Display();
+        String RandomChosenWord = randW.randomChoice();
+        System.out.println("Random word is .... " + RandomChosenWord+ " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
+        
+        
+        System.out.println("Now we are ready to play the game. "
+                + "You will have 6 attempts to choose the correct word.  ");
 
-        //boolean wordGUesseddd = false;
-
-        while (attempts != 0 && lettersLeft != 0) {
-            for (int i = 0; i < secretWord.length(); i++) {
-                System.out.print(hidden[i] + " ");
-            }
-
-            System.out.println("\nAttempts left = " + attempts);
-            System.out.println("Enter a letter to guess the word: ");
-            //User enters character
-            Scanner scan2 = new Scanner(System.in);
-            String letter = scan2.next();
-            letter = letter.toLowerCase();
-            char letterOne = letter.charAt(0);
-
-            boolean exsist = false;
-
-            for (int i = 0; i < secretWord.length(); i++) {
-                //System.out.println(", THis is the user Guess -> " + letterOne + ", THis is the current selection -> " + secretWord.charAt(i));
-
-                if (letterOne == secretWord.charAt(i)) {
-                    hidden[i] = letterOne;
-                    exsist = true;
-                    lettersLeft--;
-                    //break;
-                }
-
-            }
-
-            if (exsist == false) {
-                attempts--;
-
-            }
-            
-            boolean wordGUesseddd = true;
-            if (lettersLeft == 1) {
-                for (int i = 0; i < secretWord.length(); i++) {
-                    if (hidden[i] == '*') {
-                        wordGUesseddd = false;
-                        //System.out.println("THis is the end.");
-                        //break;
-                    }
-
-                }
-            }
-            
-            
-            if ((wordGUesseddd == true && lettersLeft == 1) || lettersLeft == 0 ) {
-                lettersLeft = 0;
-                System.out.println("THis is the end.");
-                getHidden();
-                break;
-            }
-            
-            
-            
-            
-        }
-    }
-
-    //This method provides us with the array that will be used to display the words
-    public void spaces() {
-        for (int i = 0; i < secretWord.length(); i++) {
-            hidden[i] = '*';
-        }
-
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (secretWord.charAt(i) == 32) {
-                //System.out.print(hidden[i]);
-                hidden[i] = '_';
-                //System.out.print(hidden[i] + " ");
-            }
-        }
-
-        for (int i = 0; i < secretWord.length(); i++) {
-            System.out.print(hidden[i] + " ");
-        }
-    }
-    
-    
-    public void getHidden(){
-        for (int i = 0; i < secretWord.length(); i++) {
-            System.out.print(hidden[i]);
-        }
+        
+        GuessWord guessW = new GuessWord(RandomChosenWord);
+        guessW.Guessing();
+        //guessW.spaces();
+                
     }
 
 }
