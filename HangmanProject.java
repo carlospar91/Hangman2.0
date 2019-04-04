d911a950812b68e002073b56204fbe33a2ed3628
carlospar91 <carlospar91@aol.com>
2019-04-04 13:02:19 -0500

    Update HangmanProject.java

--- HangmanProject.java
+++ HangmanProject.java
@@ -18,8 +18,8 @@ public class HangmanProject {
     public static void main(String[] args) {
         int choice1;
 
-        System.out.println("Please choose a topic: 1 for cars, 2 for presidents, 3 for a random topic."
-                + "\nIf your input is invalid we will choose a word for you!");
+        System.out.print("Please choose a topic: 1 for cars, 2 for presidents, 3 for a random topic."
+                + "\nIf your input is invalid we will choose a word for you!  ");
         Scanner sc1 = new Scanner(System.in);
         choice1 = sc1.nextInt();
         //sc1.close(); //@Nicole Deleted scanner close due to error: NoSuchElementException
@@ -29,11 +29,11 @@ public class HangmanProject {
         RandomWord randW = new RandomWord(choice1);
         randW.Display();
         String RandomChosenWord = randW.randomChoice();
-        System.out.println("Random word is .... " + RandomChosenWord+ " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
+        System.out.println("\nRandom word is .... " + RandomChosenWord+ " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
         
         
         System.out.println("Now we are ready to play the game. "
-                + "You will have 6 attempts to choose the correct word.  ");
+                + "You will have 6 attempts to choose the correct word.  "+ "\n");
 
         
         GuessWord guessW = new GuessWord(RandomChosenWord);
