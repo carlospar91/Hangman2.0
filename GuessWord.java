73a50c83b95a9d3049591a01cc1db4a62d87975c
carlospar91 <carlospar91@aol.com>
2019-04-04 13:02:04 -0500

    Update GuessWord.java

--- GuessWord.java
+++ GuessWord.java
@@ -13,10 +13,11 @@ public class GuessWord {
 
     String secretWord;
     int attempts = 6;
+    int totalAttempt = 0;
     int position = 0;
     int lettersLeft;
     char[] hidden;
-    char[] guessedLetters = new char[26];
+    char[] guessedLetters = new char[100];
 
     public GuessWord(String word) {
         secretWord = word.toLowerCase();
@@ -24,71 +25,72 @@ public class GuessWord {
         lettersLeft = secretWord.length();
     }
 
-    
     public void Guessing() {
-        int lettersLeft = secretWord.length();
-//      char[] hidden = new char[secretWord.length()];
+        //int lettersLeft = secretWord.length();
+        Picture hangPic = new Picture();
+        
+        //hangPic.hangmanImage(secretWord, attempts);
         spaces();
-        System.out.println(attempts);
-
-        //boolean wordGUesseddd = false;
-
+        
         while (attempts != 0 && lettersLeft != 0) {
-            for (int i = 0; i < secretWord.length(); i++) {
-                System.out.print(hidden[i] + " ");
-            }
+            
+           totalAttempt++;
+            getHidden();
 
-            System.out.println("\nAttempts left = " + attempts);
-            System.out.println("Enter a letter to guess the word: ");
+            hangPic.hangmanImage(secretWord, attempts);
+            
+            //System.out.println("\nAttempts left = " + attempts);
+            System.out.print("Enter a letter to guess the word: ");
             //User enters character
             Scanner scan2 = new Scanner(System.in);
             String letter = scan2.next();
             letter = letter.toLowerCase();
             char letterOne = letter.charAt(0);
+            //alreadyGuessed(letterOne);
+            //verifies the the user inputs the correct value, This will not take in multiple values or non caracters, it will take in capital lettes
+            while (letterOne < 97 || letterOne > 122 || letter.length() > 1 ) {
+                totalAttempt++;
+                System.out.print("Your input was incorrect. Please Enter another letter : ");
+                letter = scan2.next();
+                letter = letter.toLowerCase();
+                letterOne = letter.charAt(0);
+                //alreadyGuessed(letterOne);
+                
+                
+            }
 
             boolean exsist = false;
-
             for (int i = 0; i < secretWord.length(); i++) {
-                //System.out.println(", THis is the user Guess -> " + letterOne + ", THis is the current selection -> " + secretWord.charAt(i));
-
                 if (letterOne == secretWord.charAt(i)) {
                     hidden[i] = letterOne;
                     exsist = true;
                     lettersLeft--;
-                    //break;
                 }
-
             }
 
             if (exsist == false) {
                 attempts--;
-
             }
-            
+
             boolean wordGUesseddd = true;
             if (lettersLeft == 1) {
                 for (int i = 0; i < secretWord.length(); i++) {
                     if (hidden[i] == '*') {
                         wordGUesseddd = false;
-                        //System.out.println("THis is the end.");
-                        //break;
                     }
-
                 }
             }
-            
-            
-            if ((wordGUesseddd == true && lettersLeft == 1) || lettersLeft == 0 ) {
+
+            if ((wordGUesseddd == true && lettersLeft == 1) || lettersLeft == 0) {
                 lettersLeft = 0;
                 System.out.println("THis is the end.");
+                hangPic.hangmanImage(secretWord, attempts);
                 getHidden();
                 break;
             }
-            
-            
-            
-            
         }
+        
+        hangPic.hangmanImage(secretWord, attempts);
     }
 
     //This method provides us with the array that will be used to display the words
@@ -105,16 +107,33 @@ public class GuessWord {
             }
         }
 
+//        for (int i = 0; i < secretWord.length(); i++) {
+//            System.out.print(hidden[i] + " ");
+//        }
+    }
+
+    public void getHidden() {
         for (int i = 0; i < secretWord.length(); i++) {
             System.out.print(hidden[i] + " ");
         }
+        System.out.println();
     }
-    
-    
-    public void getHidden(){
-        for (int i = 0; i < secretWord.length(); i++) {
-            System.out.print(hidden[i]);
+
+    public boolean alreadyGuessed(char c) {
+        boolean result = false;
+        for (int i = 0; i < 100; i++) {
+            
+            if (guessedLetters[i] == c) {
+                System.out.println("\nThis Letter has already been Guessed!");
+                result = true;
+                break;
+            } else {
+                guessedLetters[totalAttempt] = c;
+                result = false;
+            }
+
         }
+        return result;
     }
 
 }
