d5578aa1cf2e05a6cbdc80924f729f18b47f0d86
carlospar91 <carlospar91@aol.com>
2019-04-05 11:48:04 -0500

    Update HangmanProject.java

--- HangmanProject.java
+++ HangmanProject.java
@@ -24,22 +24,50 @@ public class HangmanProject {
         choice1 = sc1.nextInt();
         //sc1.close(); //@Nicole Deleted scanner close due to error: NoSuchElementException
 
-        
-        
         RandomWord randW = new RandomWord(choice1);
         randW.Display();
         String RandomChosenWord = randW.randomChoice();
-        System.out.println("\nRandom word is .... " + RandomChosenWord+ " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
-        
-        
-        System.out.println("Now we are ready to play the game. "
-                + "You will have 6 attempts to choose the correct word.  "+ "\n");
+        System.out.println("\nRandom word is .... " + RandomChosenWord + " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
 
-        
+        System.out.println("Now we are ready to play the game. "
+                + "You will have 6 attempts to choose the correct word.  " + "\n");
+
         GuessWord guessW = new GuessWord(RandomChosenWord);
         guessW.Guessing();
-        //guessW.spaces();
-                
+
+        System.out.print("Would you like to play again? Enter Y or N ");
+        String playAgain = sc1.next();
+
+        playAgain = playAgain.toUpperCase();
+        char cPlay = playAgain.charAt(0);
+
+        if (cPlay == 'Y') {
+
+            System.out.print("Please choose a topic: 1 for cars, 2 for presidents, 3 for a random topic."
+                    + "\nIf your input is invalid we will choose a word for you!  ");
+            //Scanner sc1 = new Scanner(System.in);
+            choice1 = sc1.nextInt();
+            randW = new RandomWord(choice1);
+            randW.Display();
+            RandomChosenWord = randW.randomChoice();
+            System.out.println("\nRandom word is .... " + RandomChosenWord + " .. This line is for debugging purposes it wont show up once the game is ready to go.\n");
+
+            System.out.println("Now we are ready to play the game. "
+                    + "You will have 6 attempts to choose the correct word.  " + "\n");
+
+            guessW = new GuessWord(RandomChosenWord);
+            guessW.Guessing();
+
+            System.out.print("Would you like to play again? Enter Y or N ");
+            playAgain = sc1.next();
+
+            playAgain = playAgain.toUpperCase();
+            cPlay = playAgain.charAt(0);
+
+        } 
+
+            System.out.println("Thank you for playing. This program has been closed gracefully.");
+        
     }
 
 }
