91628925e928528d37259ba182abfa4673e96b37
carlospar91 <carlospar91@aol.com>
2019-04-05 10:05:45 -0500

    Update GuessWord.java

--- GuessWord.java
+++ GuessWord.java
@@ -18,6 +18,8 @@ public class GuessWord {
     int lettersLeft;
     char[] hidden;
     char[] guessedLetters = new char[100];
+    List<Character> chars = new ArrayList<>();
+    List<Character> alphabet = new ArrayList<>();
 
     public GuessWord(String word) {
         secretWord = word.toLowerCase();
@@ -26,37 +28,38 @@ public class GuessWord {
     }
 
     public void Guessing() {
-        //int lettersLeft = secretWord.length();
         Picture hangPic = new Picture();
-        
+
         //hangPic.hangmanImage(secretWord, attempts);
         spaces();
-        
+        createAlphabet();
+        printListChar();
+
         while (attempts != 0 && lettersLeft != 0) {
-            
-           totalAttempt++;
+            boolean alreadyexisit = false;
+            totalAttempt++;
             getHidden();
 
             hangPic.hangmanImage(secretWord, attempts);
-            
-            //System.out.println("\nAttempts left = " + attempts);
+            printListChar();
+
             System.out.print("Enter a letter to guess the word: ");
             //User enters character
             Scanner scan2 = new Scanner(System.in);
             String letter = scan2.next();
             letter = letter.toLowerCase();
             char letterOne = letter.charAt(0);
-            //alreadyGuessed(letterOne);
+            alreadyexisit = listChar(letterOne);
+
             //verifies the the user inputs the correct value, This will not take in multiple values or non caracters, it will take in capital lettes
-            while (letterOne < 97 || letterOne > 122 || letter.length() > 1 ) {
+            while (letterOne < 97 || letterOne > 122 || letter.length() > 1 || alreadyexisit == false) {
                 totalAttempt++;
                 System.out.print("Your input was incorrect. Please Enter another letter : ");
                 letter = scan2.next();
                 letter = letter.toLowerCase();
                 letterOne = letter.charAt(0);
-                //alreadyGuessed(letterOne);
-                
-                
+                alreadyexisit = listChar(letterOne);
+
             }
 
             boolean exsist = false;
@@ -89,8 +92,11 @@ public class GuessWord {
                 break;
             }
         }
-        
+
         hangPic.hangmanImage(secretWord, attempts);
+        printListChar();
+        System.out.println("This is the total number of attempts taken>>> : >>" + totalAttempt);
+
     }
 
     //This method provides us with the array that will be used to display the words
@@ -101,15 +107,9 @@ public class GuessWord {
 
         for (int i = 0; i < secretWord.length(); i++) {
             if (secretWord.charAt(i) == 32) {
-                //System.out.print(hidden[i]);
                 hidden[i] = '_';
-                //System.out.print(hidden[i] + " ");
             }
         }
-
-//        for (int i = 0; i < secretWord.length(); i++) {
-//            System.out.print(hidden[i] + " ");
-//        }
     }
 
     public void getHidden() {
@@ -119,21 +119,56 @@ public class GuessWord {
         System.out.println();
     }
 
-    public boolean alreadyGuessed(char c) {
-        boolean result = false;
-        for (int i = 0; i < 100; i++) {
-            
-            if (guessedLetters[i] == c) {
-                System.out.println("\nThis Letter has already been Guessed!");
-                result = true;
-                break;
-            } else {
-                guessedLetters[totalAttempt] = c;
-                result = false;
-            }
-
+    public boolean listChar(char ch) {
+        boolean ans = alphabet.contains(ch);
+        int index = ch - 97;
+        if (ans) {
+            System.out.println("The list contains " + ch);
+            alphabet.remove(index);
+            ans = true;
+        } else {
+            System.out.println("The list does not contains " + ch);
+            ans = false;
+            System.out.println("This guess already exisist!!");
         }
-        return result;
+        return ans;
+    }
+
+    public void printListChar() {
+
+        System.out.println("These are the letters you have remaining to choose from:");
+
+        System.out.println(alphabet);
+
+    }
+
+    public void createAlphabet() {
+        alphabet.add('a');
+        alphabet.add('b');
+        alphabet.add('c');
+        alphabet.add('d');
+        alphabet.add('e');
+        alphabet.add('f');
+        alphabet.add('g');
+        alphabet.add('h');
+        alphabet.add('i');
+        alphabet.add('j');
+        alphabet.add('k');
+        alphabet.add('l');
+        alphabet.add('m');
+        alphabet.add('n');
+        alphabet.add('o');
+        alphabet.add('p');
+        alphabet.add('q');
+        alphabet.add('r');
+        alphabet.add('s');
+        alphabet.add('t');
+        alphabet.add('u');
+        alphabet.add('v');
+        alphabet.add('w');
+        alphabet.add('x');
+        alphabet.add('y');
+        alphabet.add('z');
     }
 
 }
